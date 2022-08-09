06637a3a7ea19f38096feeaa961a4771eac4a47a
Juan Pablo <jpgallegos20@gmail.com>
2022-08-09 03:46:58 -0300

    4-WIP-Create the home screen
    - Add a list of coins
    - Add layout to the Home Screen

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,38 +1,163 @@
 import { TextLink } from 'solito/link'
-import React from 'react'
-import { Center, HStack, Text, Heading, Link, VStack, Box } from 'native-base'
+import React, { useEffect, useState } from 'react'
+import {
+  Center,
+  HStack,
+  Text,
+  Heading,
+  VStack,
+  Box,
+  FlatList,
+  Avatar,
+  Pressable,
+} from 'native-base'
+import { ICryptoCurrency } from 'http/types/cryptocoins'
+import { isWeb } from 'app/constants'
+import cryptocoinsService from 'http/services/cryptocoin.service'
 
-export function HomeScreen({ route }: { route: any }) {
+type Props = {
+  route: {
+    params: {
+      coins: ICryptoCurrency[]
+    }
+  }
+}
+
+export function HomeScreen({ route }: Props) {
+  const [coinsData, setCoinsData] = useState<ICryptoCurrency[]>([])
   const coins = route.params?.coins
+  useEffect(() => {
+    const getCoinsData = async () => {
+      await cryptocoinsService
+        .getCoins()
+        .then((response: any) => {
+          setCoinsData(response)
+        })
+        .catch((error) => console.log(error))
+    }
+    getCoinsData()
+  }, [])
 
-  console.log(coins)
   return (
-    <Center>
-      <VStack alignItems="center" space="md">
-        <Heading>NativeBase + Solito ❤️</Heading>
-        <HStack alignItems="center" space="sm">
-          <Link href="https://solito.dev/" isExternal>
-            <Text
-              _light={{ color: 'gray.700' }}
-              _dark={{ color: 'gray.400' }}
-              underline
-              fontSize={'xl'}
-            >
-              Learn Solito
-            </Text>
-          </Link>
-          <Text>/</Text>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={'xl'}>
-              Learn NativeBase
-            </Text>
-          </Link>
-        </HStack>
+    <Center width="full">
+      <VStack alignItems="center" space="md" width="100%">
+        <Box padding={2}>
+          <Heading
+            size="xl"
+            textAlign="center"
+            color="green.600"
+            marginBottom={8}
+            textTransform="uppercase"
+          >
+            Top Cryptocurrency Prices
+          </Heading>
+        </Box>
+
+        <VStack width="80%">
+          <HStack
+            space={4}
+            justifyContent="space-between"
+            width="full"
+            borderBottomWidth="1"
+            borderColor="black"
+            padding={2}
+            background="gray.300"
+          >
+            <Box flex={isWeb ? '0 0 10%' : 0}>
+              <Text>#</Text>
+            </Box>
+
+            <Box flex={isWeb ? '0 0 25%' : 0}>
+              <Text textTransform="uppercase">Name</Text>
+            </Box>
+
+            <Box flex={isWeb ? '0 0 15%' : 0}>
+              <Text textTransform="uppercase">Price</Text>
+            </Box>
+
+            <Box flex={isWeb ? '0 0 15%' : 0}>
+              <Text textTransform="uppercase">24h Change</Text>
+            </Box>
+
+            <Box flex={isWeb ? '0 0 15%' : 0}>
+              <Text textTransform="uppercase">Market Cap</Text>
+            </Box>
+          </HStack>
+          <FlatList
+            data={coinsData}
+            renderItem={({ item }) => {
+              console.log(item.rank)
+              return (
+                <Pressable _hover={{ bg: 'gray.200' }}>
+                  <HStack
+                    space={4}
+                    alignItems="center"
+                    justifyContent="space-between"
+                    width="full"
+                    borderBottomWidth="1"
+                    borderColor="black"
+                    padding={2}
+                  >
+                    <Box flex={isWeb ? '0 0 10%' : 0}>
+                      <Text>{item.rank}</Text>
+                    </Box>
+
+                    <HStack
+                      space={4}
+                      flex={isWeb ? '0 0 25%' : 0}
+                      alignItems="center"
+                    >
+                      <Box>
+                        <Avatar size="36px" source={{ uri: item?.logo_url }} />
+                      </Box>
+
+                      <Box>
+                        <Text>{item?.name}</Text>
+                        <Text>{item?.currency}</Text>
+                      </Box>
+                    </HStack>
+
+                    <Box flex={isWeb ? '0 0 10%' : 0}>
+                      <Text>${Number(item?.price).toFixed()}</Text>
+                    </Box>
+
+                    <Box flex={isWeb ? '0 0 10%' : 0}>
+                      <Text
+                        color={
+                          !item['1d']?.price_change_pct.includes('-')
+                            ? 'green.600'
+                            : 'red.400'
+                        }
+                      >
+                        {
+                          +(
+                            (item['1d']
+                              ?.price_change_pct as unknown as number) * 100
+                          ).toFixed(2)
+                        }
+                        %
+                      </Text>
+                    </Box>
+
+                    {isWeb && (
+                      <Box flex={isWeb ? '0 0 10%' : 0}>
+                        <Text>
+                          {item?.currency.includes('BTC') ||
+                          item?.currency.includes('ETH')
+                            ? item?.market_cap.slice(0, 3)
+                            : item?.market_cap.slice(0, 2)}
+                           B
+                        </Text>
+                      </Box>
+                    )}
+                  </HStack>
+                </Pressable>
+              )
+            }}
+            keyExtractor={(item, index) => (isWeb ? item.id : index.toString())}
+          />
+        </VStack>
       </VStack>
-      <Box mt="6">
-        <TextLink href="/user/fernando">Regular Link</TextLink>
-        <TextLink href="/login">Login</TextLink>
-      </Box>
     </Center>
   )
 }
--- packages/http/services/cryptocoin.service.ts
+++ packages/http/services/cryptocoin.service.ts
@@ -1,12 +1,11 @@
 import http from 'http/utils/axios'
-import type { ICryptoCoins } from 'http/types/cryptocoins'
+import type { ICryptoCurrency } from 'http/types/cryptocoins'
 
 const cryptocoinsService = {
   getCoins: () => {
-    return http<ICryptoCoins>({
+    return http<ICryptoCurrency>({
       method: 'GET',
-      url: `https://api.nomics.com/v1/currencies/ticker?key=${process.env.NOMICS_KEY}&ids=BTC,ETH,XRP&convert=USD`,
-
+      url: `https://api.nomics.com/v1/currencies/ticker?key=2725b144ddc4853f6c8ce5754799c4d48d1cf532&ids=BTC,ETH,XRP,USDT,USDC,BNB,ADA,BUSD,SOL,DOT&convert=USD`,
       validateStatus: (status) => {
         return status >= 200 && status < 300
       },
